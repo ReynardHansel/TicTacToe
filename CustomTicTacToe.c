91e831a35661f2244c2a9d295bfda8f3f42bbdfe
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2022-10-05 12:45:44 +0700

    changed inputCharacter to be a function instead of running in main

--- CustomTicTacToe.c
+++ CustomTicTacToe.c
@@ -4,17 +4,20 @@ char board [3][3];
 char PLAYER;
 char CPU = 'X';
 
-
+void inputCharacter();
 
 int main (){
+    inputCharacter();
+
+
+    return 0;
+}
+
+void inputCharacter(){
     printf("Enter your character: ");
     scanf("%c", &PLAYER);
     if (PLAYER == 'X')
     {
         CPU = 'O';
     }
-
-    
-
-    return 0;
 }
